f4f86d6cdeb841e110685d1de25c7d5d13cf082b
x-pes Test Project <xpes@lexoro.tech>
2019-11-27 15:53:17 +0200

    Added docker.sh and change the run script

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 nohup sh -c "python3 -m tensorboard.main --logdir=/artifacts/log --port=25864" > /artifacts/log_tensorboard &
-python3 train.py --dataset blender --dataset_folder abc_test --log_dir /artifacts/log --batch_size 13 --max_epoch 200 --dump_dir /artifacts/eval
+CUDA_VISIBLE_DEVICES=1 python3 train.py --dataset blender --dataset_folder abc6_small --log_dir /artifacts/log --batch_size 13 --max_epoch 200 --dump_dir /artifacts/eval
